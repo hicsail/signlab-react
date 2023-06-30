940a3280b4a58896a56f5933cc9e6f468f86fa05
ukamila <ukamila@bu.edu>
2023-06-30 14:18:36 -0400

    changed themeprovider

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider as MuiThemeProvider, createTheme } from '@mui/material/st
 export interface ThemeProviderProps {
   children: React.ReactNode;
 }
-
 export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
   const theme = createTheme({
     palette: {
