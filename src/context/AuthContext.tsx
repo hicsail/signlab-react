8297ddf3dab5f27ecef2e2fead51b824ed541c17
Kamila <ukamila@bu.edu>
2023-07-17 16:45:43 -0400

    working on context for current project and study

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,14 @@
+import { createContext } from 'react';
+import { User } from '../hooks/useUser';
+
+interface AuthContext {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+const AuthContext = createContext<AuthContext>({
+  user: null,
+  setUser: () => {}
+});
+
+export { AuthContext };
