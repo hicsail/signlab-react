0e09240093447f37609f3b139e6be007877f323d
Kamila <ukamila@bu.edu>
2023-08-02 11:50:51 -0400

    dataset controls page

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { ProjectUserPermissions } from './pages/projects/ProjectUserPermissions'
 import { StudyUserPermissions } from './pages/studies/UserPermissions';
 import { DownloadTags } from './pages/studies/DownloadTags';
 import { LoginPage } from './pages/LoginPage';
+import { DatasetControls } from './pages/datasets/DatasetControls';
 
 function App() {
   return (
@@ -28,13 +29,14 @@ function App() {
             <Route path={'/loginpage'} element={<LoginPage />} />
             <Route path={'/newproject'} element={<NewProject />} />
             <Route path={'/projectcontrol'} element={<ProjectControl />} />
-            <Route path={'/projectaccess'} element={<ProjectAccess />} />
             <Route path={'/userpermissions'} element={<ProjectUserPermissions />} />
             <Route path={'/newstudy'} element={<NewStudy />} />
             <Route path={'/studycontrol'} element={<StudyControl />} />
             <Route path={'/studyuserpermissions'} element={<StudyUserPermissions />} />
             <Route path={'/downloadtags'} element={<DownloadTags />} />
             <Route path={'/successpage'} element={<SuccessPage />} />
+            <Route path={'/datasetcontrols'} element={<DatasetControls />} />
+            <Route path={'/projectaccess'} element={<ProjectAccess />} />
             <Route path={'/contribute'} element={<ContributePage />} />
             <Route path={'/tagging'} element={<TagView />} />
           </Routes>
---
+++ src/components/DatasetControlComponent.tsx
@@ -0,0 +1,91 @@
+import { Box } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { GridRowModesModel } from '@mui/x-data-grid-pro';
+import { useState } from 'react';
+
+interface Row {
+  id: number;
+  view: string;
+  entry: string;
+  responder: string;
+  access: boolean;
+}
+
+interface Table {
+  tableRows: Row[];
+}
+
+const DatasetControlComponent: React.FC<Table> = ({ tableRows }: Table) => {
+  const [rows] = useState(tableRows);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  /*const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };*/
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 180 },
+    {
+      field: 'view',
+      headerName: 'View',
+      width: 140,
+      editable: true
+    },
+    {
+      field: 'entry',
+      headerName: 'Entry ID',
+      width: 200,
+      editable: true
+    },
+    {
+      field: 'responder',
+      headerName: 'Responder ID',
+      width: 200,
+      editable: true
+    },
+    {
+      field: 'access',
+      type: 'boolean',
+      headerName: 'Access',
+      width: 100
+    }
+  ];
+
+  return (
+    <Box sx={{ height: 800, width: '100%', boxShadow: '1px 9px 15px darkgrey' }}>
+      <DataGrid
+        sx={{
+          '&.MuiDataGrid-root': {
+            fontWeight: 'normal'
+          },
+          '& .MuiDataGrid-cell': {
+            overflow: 'auto',
+            paddingTop: '8px !important',
+            paddingBottom: '8px !important'
+          }
+        }}
+        getRowHeight={() => 'auto'}
+        rows={rows}
+        columns={columns}
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 8
+            }
+          }
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+};
+
+export { DatasetControlComponent };
