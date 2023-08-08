8efe376d94c5d2bf0658093957adddab721bac4b
Kamila <ukamila@bu.edu>
2023-08-08 16:09:56 -0400

    emptying schema/uischema

--- src/pages/studies/NewStudy.tsx
+++ src/pages/studies/NewStudy.tsx
@@ -1,103 +1,7 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { useState } from 'react';
 import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
 import { JsonForms } from '@jsonforms/react';
-
-const schema = {
-  type: 'object',
-  properties: {
-    name: {
-      type: 'string',
-      pattern: '^[a-zA-Z 0-9]*$'
-    },
-    description: {
-      type: 'string'
-    },
-    instructions: {
-      type: 'string'
-    },
-    times: {
-      type: 'string',
-      default: 1
-    },
-    tag: {
-      type: 'number'
-    }
-  },
-  required: ['name', 'description', 'instructions'],
-  errorMessage: {
-    type: 'data should be an object',
-    properties: { name: 'Study name should be ...', description: 'Description ' },
-    _: 'data should ...'
-  }
-};
-
-const uischema = {
-  type: 'Categorization',
-  elements: [
-    {
-      type: 'Category',
-      label: 'Study Identification',
-      elements: [
-        {
-          type: 'VerticalLayout',
-          elements: [
-            {
-              type: 'Control',
-              label: 'Name',
-              scope: '#/properties/name'
-            },
-            {
-              type: 'Control',
-              label: 'Description',
-              scope: '#/properties/description'
-            },
-            {
-              type: 'Control',
-              label: 'Instructions',
-              scope: '#/properties/instructions'
-            },
-            {
-              type: 'Control',
-              label: 'Number of times each entry needs to be tagged (default 1)',
-              scope: '#/properties/times'
-            }
-          ]
-        }
-      ]
-    },
-    {
-      type: 'Category',
-      label: 'Construct Tagging Interface',
-      elements: [
-        {
-          type: 'VerticalLayout',
-          elements: [
-            {
-              type: 'Control',
-              label: 'Name',
-              scope: '#/properties/name'
-            },
-            {
-              type: 'Control',
-              label: 'Description',
-              scope: '#/properties/description'
-            },
-            {
-              type: 'Control',
-              label: 'Tag',
-              scope: '#/properties/tag'
-            }
-          ]
-        }
-      ]
-    }
-  ],
-  options: {
-    variant: 'stepper',
-    showNavButtons: true
-  }
-};
 
 const NewStudy: React.FC = () => {
   const initialData = {
@@ -111,9 +15,24 @@ const NewStudy: React.FC = () => {
   const handleChange = (data: any) => {
     setData(data);
   };
+  // @ts-ignore
+  function produceJSONForm() {
+    // @ts-ignore
+    const schema = {
+      type: 'object',
+      properties: {},
+      required: []
+    };
+    // @ts-ignore
+    const uischema = {
+      type: 'VerticalLayout',
+      elements: []
+    };
+  }
 
   return (
     <Container sx={{ left: '5%', width: '90%', top: '100px', position: 'absolute' }}>
+      <Typography variant="h5">Create New Study</Typography>
       <JsonForms schema={schema} uischema={uischema} data={data} renderers={materialRenderers} cells={materialCells} onChange={({ data }) => handleChange(data)} />
     </Container>
   );
