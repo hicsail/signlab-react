51aac7a085e22ce11e571b807312d74fce1ed1ac
Kamila <ukamila@bu.edu>
2023-08-08 17:28:45 -0400

    stepper

--- src/pages/studies/NewStudy.tsx
+++ src/pages/studies/NewStudy.tsx
@@ -1,20 +1,47 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Stepper, Step, StepLabel, Button, TextField, Box, StepContent, Paper } from '@mui/material';
+import React from 'react';
 import { useState } from 'react';
-import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
-import { JsonForms } from '@jsonforms/react';
 
 const NewStudy: React.FC = () => {
-  const initialData = {
-    name: '',
-    description: '',
-    instructions: ''
+  //all constants
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [tagFields, setTagFields] = useState([]);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
-  const [data, setData] = useState(initialData);
-
-  const handleChange = (data: any) => {
-    setData(data);
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const steps = [
+    {
+      label: 'Study Identification',
+      element: <TextField required margin="dense" id="name" label="Dataset Name" fullWidth InputLabelProps={{ shrink: true }} variant="standard" />
+    },
+    {
+      label: 'Construct Tagging Interface',
+      element: (
+        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+          <Button>Upload CSV</Button>
+        </Box>
+      )
+    },
+    {
+      label: 'Select Tag Training',
+      element: (
+        <Button variant="outlined" sx={{ margin: '10px' }}>
+          Upload Videos (ZIP)
+        </Button>
+      )
+    }
+  ];
+  //all functions
   // @ts-ignore
   function produceJSONForm() {
     // @ts-ignore
@@ -33,7 +60,41 @@ const NewStudy: React.FC = () => {
   return (
     <Container sx={{ left: '5%', width: '90%', top: '100px', position: 'absolute' }}>
       <Typography variant="h5">Create New Study</Typography>
-      <JsonForms schema={schema} uischema={uischema} data={data} renderers={materialRenderers} cells={materialCells} onChange={({ data }) => handleChange(data)} />
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((step) => {
+            const stepProps: { completed?: boolean } = {};
+            const labelProps: {
+              optional?: React.ReactNode;
+            } = {};
+            return (
+              <Step key={step.label} {...stepProps}>
+                <StepLabel {...labelProps}>{step.label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext}>{activeStep === steps.length - 1 ? 'Finish' : 'Next'}</Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </Box>
     </Container>
   );
 };
