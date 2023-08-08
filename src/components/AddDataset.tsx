d4b140725a2a86bb7d3db5cc2bfae94694501550
Kamila <ukamila@bu.edu>
2023-08-08 12:11:50 -0400

    adding dataset validation

--- src/components/AddDataset.tsx
+++ src/components/AddDataset.tsx
@@ -1,10 +1,9 @@
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 interface ShowProps {
   show: boolean;
@@ -14,22 +13,41 @@ interface ShowProps {
 const AddDataset: React.FC<ShowProps> = (props: ShowProps) => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [error, setError] = useState(false);
-  const [completed, setCompleted] = useState(false);
+  //const [formErrors, setErrors] = useState({ name: '', description: '' });
+  const [nameValid, setNameValid] = useState(false);
+  const [descriptionValid, setDescriptionValid] = useState(false);
+  const [formValid, setFormValid] = useState(false);
 
-  const handleChange = (event: any, type: string) => {
-    const newValue = event.target.value;
-    if (type == 'name') {
-      setName(newValue);
-    } else {
-      setDescription(newValue);
+  const validateField = (fieldName: string, value: string) => {
+    //let fieldValidationErrors = formErrors;
+    switch (fieldName) {
+      case 'name':
+        setNameValid(value.length >= 2);
+        //fieldValidationErrors.name = nameValid ? '' : ' is invalid';
+        break;
+      case 'description':
+        setDescriptionValid(value.length >= 2);
+        //fieldValidationErrors.description = nameValid ? '' : ' is invalid';
+        break;
+      default:
+        break;
     }
-    if (name.length > 3 && description.length > 3) {
-      setError(false);
-      setCompleted(true);
+    //setErrors(fieldValidationErrors);
+    validateForm();
+  };
+
+  const validateForm = () => {
+    setFormValid(nameValid && descriptionValid);
+  };
+
+  const handleChange = (e: any, type: string) => {
+    const value = e.target.value;
+    if (type == 'name') {
+      setName(value);
+      validateField(type, value);
     } else {
-      setError(true);
-      setCompleted(false);
+      setDescription(value);
+      validateField(type, value);
     }
   };
 
@@ -37,38 +55,40 @@ const AddDataset: React.FC<ShowProps> = (props: ShowProps) => {
     <div>
       <Dialog open={props.show} onClose={props.toggleModal}>
         <DialogTitle>Create New Dataset</DialogTitle>
-        <DialogTitle sx={{ color: error ? 'red' : 'gray', fontSize: '12px', marginTop: '-20px', fontStyle: 'italic' }}>Name and Description are required</DialogTitle>
+        <DialogTitle sx={{ color: formValid ? 'gray' : 'red', fontSize: '12px', marginTop: '-20px', fontStyle: 'italic' }}>Name and Description are required</DialogTitle>
         <DialogContent>
-          <TextField
-            autoFocus
-            value={name}
-            onChange={(e) => handleChange(e, 'name')}
-            required
-            margin="dense"
-            id="name"
-            label="Dataset Name"
-            type="name"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            value={description}
-            onChange={(e) => handleChange(e, 'description')}
-            required
-            margin="dense"
-            id="description"
-            label="Dataset Description"
-            type="description"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            variant="standard"
-          />
+          <form className="addDataset">
+            <TextField
+              value={name}
+              onChange={(event) => handleChange(event, 'name')}
+              required
+              margin="dense"
+              id="name"
+              label="Dataset Name"
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+              variant="standard"
+            />
+            <TextField
+              value={description}
+              onChange={(event) => handleChange(event, 'description')}
+              required
+              margin="dense"
+              id="description"
+              label="Dataset Description"
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+              variant="standard"
+            />
+          </form>
         </DialogContent>
         <DialogActions>
-          <Button onClick={props.toggleModal}>Cancel</Button>
-          <Button onClick={completed ? props.toggleModal : () => ''}>Create</Button>
+          <button onClick={props.toggleModal} type="submit">
+            Cancel
+          </button>
+          <button disabled={!formValid} onClick={props.toggleModal} type="submit">
+            Create
+          </button>
         </DialogActions>
       </Dialog>
     </div>
